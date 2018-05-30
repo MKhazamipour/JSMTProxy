20a13c3dcdc72ff47972c9c5cf3a0050242930bc
Mormoroth <mormoroth@gmail.com>
2018-05-30 10:35:28 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i -e 's/b0cbcef5a486d9636472ac27f8e11a9d/'"$secret"'/g'  /opt/JSMTProxy/config.json
+sed -i -e 's/581bbe4b3adb6e61ec18837ac9c0b930/'"$secret"'/g'  /opt/JSMTProxy/config.json
 pm2 start /opt/JSMTProxy/mtproxy.js -i max --no-daemon
